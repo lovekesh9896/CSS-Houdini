eebf6e129aafa3a268f05b1aab30fc52b44173b8
lovekesh <kumarmehlan@gmail.com>
2021-06-02 14:50:35 +0530

    css register property created

---
+++ index.js
@@ -0,0 +1,29 @@
+// will not work in safari and edge
+
+CSS.registerProperty({
+	name: "--color-of-stroke",
+	syntax: "<color>",
+	initialValue: "red",
+	inherits: false,
+});
+
+CSS.registerProperty({
+	name: "--y-value-of-stroke",
+	syntax: "<integer>",
+	initialValue: "0",
+	inherits: false,
+});
+
+CSS.registerProperty({
+	name: "--x-value-of-stroke",
+	syntax: "<integer>",
+	initialValue: "0",
+	inherits: false,
+});
+
+CSS.registerProperty({
+	name: "--number-of-lines",
+	syntax: "<integer>",
+	initialValue: "100",
+	inherits: false,
+});
